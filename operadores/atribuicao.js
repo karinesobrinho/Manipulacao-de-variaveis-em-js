26bac9021edefc46673e74ad539a81a938fc334d
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-03 16:49:36 -0300

    atribuicao

---
+++ operadores/atribuicao.js
@@ -0,0 +1,32 @@
+a = 0
+b = 1
+c = 2
+d = 3
+e = 4
+f = 5
+g = 7
+h = 8
+i = 9
+
+//atribuicao 
+x = a
+
+//atribuicao de adicao
+x = x + e  //ou x += e
+console.log(x)
+
+//atribuicao de subtracao
+y =- f
+console.log(y)
+
+//atribuicao de divisao
+d /= c
+console.log(d)
+
+//atribuicao de multiplicacao
+b *= g
+console.log(b)
+
+//atribuicao de resto
+h %= i
+console.log(g)
