64899dffa2334762c4efaff5c42299ccf7adda22
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-03 00:26:22 -0300

    aritmeticos

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,29 @@
+var x = 10
+
+//obs: na atribuicao de novas variaveis sempre vem o sinal antes
+//incremento
+var incremento = ++x
+console.log(`incremento == x++ || ++x == ${incremento}`)
+
+//decremento
+var decremento = --x
+console.log(`decremento == x-- || --x == ${decremento}`)
+
+//negacao (-) e adicao (+)
+-3
++'3' //retorna 3
++true //retorna 1
++false //retorna 0
+-false //retorna -1
+
+//operador de agrupamento ()
+var grup = 2 * (2 + 3)
+console.log(`operador de agrupamento 2*(2+3)== ${grup}`)
+
+//exponenciacao
+var exp = x**2
+console.log(`10**2 == ${exp}`)
+
+//resto da divisao
+var resto = x%3
+console.log(`10%3 == ${resto}`)
