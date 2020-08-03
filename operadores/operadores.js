ec02322ca65cd60f21e15b44aeab50a480687516
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-02 23:50:16 -0300

    operador binario e unitario

---
+++ operadores/operadores.js
@@ -0,0 +1,7 @@
+//operador binario
+var x = 1 + 1
+console.log(`operador1 + operando + operador2 == 1+1 == ${x}`)
+
+//operador unitario
+var y = x++
+console.log(`operador1 + operando + operando == x++ == ${y}`)
