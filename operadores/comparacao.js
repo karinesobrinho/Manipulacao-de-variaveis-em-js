bda54c1ba8d37803bea22461baa9dfa35c05563d
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-03 17:08:44 -0300

    comparacao

---
+++ operadores/comparacao.js
@@ -0,0 +1,29 @@
+var x = '3'
+var y = '4'
+var z = 0
+
+// == retorna apenas o valor e nao o tipo de objeto
+var x = '3'
+console.log(3 == x)
+
+// === confere o tipo de3 objeto e nao so o valor
+console.log(3 === x)
+
+//diferente != funciona com o mesmo teste do ==
+console.log(x != 3)
+
+//diferente !==
+console.log(x !== 3)
+
+//maior >
+console.log(y > x)
+
+//menor <
+console.log(x < y)
+
+//maior igual >=
+console.log(y >= x)
+
+//menor igual <=
+console.log(x <= y)
+
